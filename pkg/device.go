9141c5a5387ab35961a7fe00f7aa450e85fb5c51
michealroberts <michael@observerly.com>
2025-11-23 21:07:32 +0000

    feat: add type Device struct to adafruit in pkg root
    feat: add type Device struct to adafruit in pkg root

--- pkg/device.go
+++ pkg/device.go
@@ -15,16 +15,10 @@ import (
 
 /**************************************************************************************/
 
-const DefaultTMP117I2CAddress uint16 = 0x48
-
-/**************************************************************************************/
-
-// NewDevice creates a new Device instance with the specified I2C address and bus.
-func NewDevice(address uint16, bus machine.I2C) *Device {
-	return &Device{
-		address: address,
-		bus:     bus,
-	}
+// Device represents an Adafruit device connected over I2C.
+type Device struct {
+	address uint16      // I2C address of the sensor
+	bus     machine.I2C // I2C bus to which the sensor is connected
 }
 
 /**************************************************************************************/
